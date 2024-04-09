16dd73649cad062e66ca5eaebf7d05ac90705ee0
jkhvacmd209 <joppe.koops@hva.nl>
2024-04-09 15:05:03 +0200

    Select a random film from the movie database and display the poster & Test server sent events

--- server.js
+++ server.js
@@ -4,11 +4,100 @@ import { logger } from '@tinyhttp/logger';
 import fs from 'node:fs';
 import ejs from 'ejs';
 import sirv from 'sirv';
+import cors from 'cors';
 
 const app = new App();
 
 app.set('view engine', 'ejs');
 app.engine('ejs', ejs.renderFile);
+app.use(cors());
+
+const getMoviePoster = async (id) => {
+  try{
+    const url = `https://api.themoviedb.org/3/movie/${id}/images?adult=false`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer ' + process.env.MOVIEDB_API_KEY
+      }
+    };
+
+    const response = await fetch(url, options);
+
+    const json = await response.json();
+
+    const poster = json.posters.find(poster => poster.iso_639_1 === null);
+    //const poster = json.backdrops[0];
+
+    if(poster) {
+      return poster;
+    } else {
+      return null
+    }
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
+
+const getMovie = async (id, includeImage) => {
+  try{
+    const url = `https://api.themoviedb.org/3/movie/${id}?adult=false`;
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer ' + process.env.MOVIEDB_API_KEY
+      }
+    };
+
+    const response = await fetch(url, options);
+
+    const json = await response.json();
+
+    if(includeImage) {
+
+      const poster = await getMoviePoster(json.id);
+
+      json.empty_poster = poster;
+
+    }
+
+    console.log(json);
+
+    return json;
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
+
+const getLatesMovieId = async () => {
+  try{
+    const movie = await getMovie('latest', false);
+    return movie.id;
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
+
+//const latestMovieId = await getLatesMovieId();
+
+const getRandomMovie = async () => {
+  try{
+
+    const randomMovieId = Math.floor(Math.random() * latestMovieId);
+
+    const movie = await getMovie(randomMovieId, true);
+
+    return movie;
+  }
+  catch (err) {
+    console.error(err);
+  }
+}
 
 app
   .use(logger())
@@ -17,4 +106,54 @@ app
 
 app.get('/', (req, res) => {
     res.render('pages/index');
+});
+
+app.get('/status', (req, res) => res.json({clients: clients.length}));
+
+let clients = [];
+let facts = [];
+
+const eventsHandler = (req, res, next) => {
+  const headers = {
+    'Content-Type': 'text/event-stream',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache'
+  };
+  res.writeHead(200, headers);
+
+  const data = `data: ${JSON.stringify(facts)}\n\n`;
+
+  res.write(data);
+
+  const clientId = Date.now();
+
+  const newClient = {
+    id: clientId,
+    res
+  };
+
+  clients.push(newClient);
+
+  //setInterval(() => sendEventsToAll(),3000);
+
+  req.on('close', () => {
+    console.log(`${clientId} Connection closed`);
+    clients = clients.filter(client => client.id !== clientId);
+  });
+}
+
+const sendEventsToAll = () => {
+  clients.forEach(client => client.res.write(`data: test\n\n`))
+}
+
+app.get('/events', eventsHandler);
+
+app.get('/player', (req, res) => {
+  res.render('pages/player');
+});
+
+app.get('/posters/', async (req, res) => {
+  const movie = await getRandomMovie(req.params.id);
+
+  res.render('pages/posters', {data: movie});
 });
